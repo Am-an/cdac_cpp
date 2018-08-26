ac1eb68bbd092459ef869092b8b520ba705718ca
Aman Agrawal <amanshivpuri96@gmail.com>
2018-08-26 17:39:19 +0530

    My second commit

---
+++ even_odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  printf("Enter the no. to check:");
+  scanf("%d",&a);
+  if(a%2==0)
+    printf("NO. is even");
+  else
+     printf("no. is odd");
+   
+   return 0;
+}
